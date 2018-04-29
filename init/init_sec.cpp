8e0b5dd3e79f92ff48c3633f51b31e4b5f7f65d1
Oves Shaikh <30339311+Oves786@users.noreply.github.com>
2018-04-29 10:42:07 +0530

    j7xelte:Simplify init... @redking12

--- init/init_sec.cpp
+++ init/init_sec.cpp
@@ -37,17 +37,15 @@ void vendor_load_properties()
 {
     const std::string bootloader = android::base::GetProperty("ro.bootloader", "");
     const std::string bl_model = bootloader.substr(0, MODEL_NAME_LEN);
-    const std::string bl_build = bootloader.substr(BUILD_NAME_LEN);
-    const std::string device = "j7xelte";
-    
+    const std::string bl_build = bootloader.substr(MODEL_NAME_LEN);
+	const std::string device = "j7xelte";
+	
     std::string model;
     std::string name;
-    std::string description;
-    std::string fingerprint;
 
     model = "SM-" + bl_model;
 
-   for (size_t i = 0; i < TOTAL_VARIANTS; i++) {
+    for (size_t i = 0; i < TOTAL_VARIANTS; i++) {
         if (model.compare(all_variants[i]->model) == 0) {
             name = all_variants[i]->codename;
         }
@@ -55,25 +53,20 @@ void vendor_load_properties()
 
     if (name.size() == 0) {
         LOG(ERROR) << "Could not detect codename, forcing j7xeltexx";
-        name = "j7xeltexx";
+        name = "j7xeltedd";
     }
 
-    description = name + "-user 7.0 NRD90M " + bl_model + bl_build + " release-keys";
-    fingerprint = "samsung/" + name + "/" + device + ":7.0/NRD90M/" + bl_model + bl_build + ":user/release-keys";
 
     LOG(INFO) << "Found bootloader: %s", bootloader.c_str();
     LOG(INFO) << "Setting ro.product.model: %s", model.c_str();
     LOG(INFO) << "Setting ro.product.device: %s", device.c_str();
     LOG(INFO) << "Setting ro.product.name: %s", name.c_str();
     LOG(INFO) << "Setting ro.build.product: %s", device.c_str();
-    LOG(INFO) << "Setting ro.build.description: %s", description.c_str();
-    LOG(INFO) << "Setting ro.build.fingerprint: %s", fingerprint.c_str();
 
     property_override_dual("ro.product.model", "ro.vendor.product.model", model.c_str());
     property_override_dual("ro.product.device", "ro.vendor.product.device", device.c_str());
     property_override_dual("ro.product.name", "ro.vendor.product.name", name.c_str());
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", fingerprint.c_str());
-    property_override("ro.build.product", device.c_str());
-    property_override("ro.build.description", description.c_str());
-}
 
+    property_override("ro.build.product", device.c_str());
+
+}
